272ba132ba22731cf0464f8739ad959fe95f234a
hadiadeel123456 <hadiadeel1298@proton.me>
2026-09-10 02:30:46 -0700

    Set bot version to 1.21.11

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const botArgs = {
   host: 'playnextsmp.aternos.me', 
   port: 25565,                         
   username: 'AFK_Bot_247',
-  version: false
+  version: 1.21.11
 };
 
 function createBot() {
