7d97a951f8996c472dca3934c6842a3ee9b4f369
hadiadeel123456 <hadiadeel1298@proton.me>
2026-09-10 02:01:09 -0700

    Initialize bot and express server in index.js

---
+++ index.js
@@ -0,0 +1,26 @@
+const mineflayer = require('mineflayer');
+const express = require('express');
+const app = express();
+
+const botArgs = {
+  host: 'playnextsmp.aternos.me', 
+  port: 25565,                         
+  username: 'AFK_Bot_247',
+  version: false
+};
+
+function createBot() {
+  const bot = mineflayer.createBot(botArgs);
+  bot.on('login', () => console.log(`[Bot] Connected to Aternos!`));
+  bot.on('spawn', () => {
+    setInterval(() => {
+      bot.setControlState('jump', true);
+      setTimeout(() => bot.setControlState('jump', false), 500);
+    }, 30000);
+  });
+  bot.on('end', () => setTimeout(createBot, 10000));
+}
+createBot();
+
+app.get('/', (req, res) => res.send('Bot is Alive!'));
+app.listen(process.env.PORT || 3000);
